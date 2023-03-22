33e10c88555f5b8cd7f509c5fae11d0aa7bbd8ec
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2023-03-22 10:49:08 +0100

    updated v2

---
+++ posts/welcome/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'My First Post',
+    description: 'This is my first post',
+    date: '2023-03-22',
+    slug: 'my-first-post',
+    banner: 'https://example.com/banner.jpg',
+    tags: ['React', 'NextJS']
+}
